de9cb9c918e616b9d2344420f92f6cf3e3d7fc54
Yerko Palma <yerko.palma@usach.cl>
2019-06-13 14:57:09 -0400

    improve feed list styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { distanceInWordsToNow } from 'date-fns';
 
 export interface FeedData {
   id: number;
   title: string;
+  author: string;
   url: string;
   date: Date;
   visible: boolean;
@@ -15,14 +17,16 @@ export interface FeedData {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'title', 'date'];
+  displayedColumns: string[] = ['title', 'date', 'controls'];
   dataSource: MatTableDataSource<FeedData>;
+  format = distanceInWordsToNow;
 
   constructor() {
     const data: FeedData[] = [{
       id: 1,
       title: 'Example feed',
       url: '',
+      author: 'Yerko',
       date: new Date(),
       visible: true
     }];
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from '../home/home.component';
@@ -10,7 +12,9 @@ import { HomeComponent } from '../home/home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
