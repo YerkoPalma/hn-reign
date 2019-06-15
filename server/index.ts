1f00f47238984b2a6b116ebfe6c17aaba3b2b7b6
Yerko Palma <yerko.palma@usach.cl>
2019-06-14 20:52:11 -0400

    update every hour

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Request, Response } from 'express';
 import * as mongodb from 'mongodb';
+import * as cron from 'node-cron';
 import * as cors from 'cors';
 import { FeedData } from '../src/app/pages/home/home.component';
 const { MongoClient } = mongodb;
@@ -32,31 +33,44 @@ class App {
     this.db = client.db('local');
   }
 
+  private async updateFeeds(): Promise<FeedData[]> {
+    const feeds: FeedData[] = [];
+    // query API
+    const { data } = await axios.get(HN_API);
+    for (const hit of data.hits) {
+      feeds.push({
+        _id: hit.story_id,
+        title: hit.story_title || hit.title,
+        url: hit.story_url || hit.url,
+        author: hit.author,
+        date: new Date(hit.created_at),
+        visible: true
+      });
+    }
+    // and save data
+    try {
+      await this.db.collection('feeds').insertMany(feeds, { ordered: false });
+    } catch (e) {}
+    return feeds;
+  }
+
   private routes(): void {
     const router = express.Router();
 
     router.get('/api/feed', async (req: Request, res: Response) => {
-      const data = await this.db.collection('feeds').find().toArray();
+      const data = await this.db.collection('feeds').find().sort({ date: 1 }, -1).toArray();
       res.status(200).send(data);
     });
 
+    // update every hour
+    cron.schedule('0 * * * *', async () => {
+      await this.updateFeeds();
+      console.log('Added new feeds!');
+    });
+
     router.post('/api/feed', async (req: Request, res: Response) => {
-      const feeds: FeedData[] = [];
-      // query API
-      const { data } = await axios.get(HN_API);
-      for (const hit of data.hits) {
-        feeds.push({
-          _id: hit.story_id,
-          title: hit.story_title || hit.title,
-          url: hit.story_url || hit.url,
-          author: hit.author,
-          date: new Date(hit.created_at),
-          visible: true
-        });
-      }
-      // and save data
-      await this.db.collection('feeds').insertMany(feeds);
-      res.status(200).send(data);
+      const feeds: FeedData[] = await this.updateFeeds();
+      res.status(200).send(feeds);
     });
 
     this.app.use('/', router);
